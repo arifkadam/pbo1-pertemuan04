9743a8e8b9ea3a229144e4672bb809414c5d10bc
arifkadam <62461132+arifkadam@users.noreply.github.com>
2020-04-25 09:28:08 +0700

    dowhile sedang mengisi gelas : isi gelas sekarang :

--- src/perulangan/programdowhile.java
+++ src/perulangan/programdowhile.java
@@ -1,6 +1,6 @@
 package perulangan;
 
-public class programwhile {
+public class programdowhile {
     public static void main(String[] args) {
         int isiGelas = 0;
         int penuh = 225;
@@ -8,14 +8,16 @@ public class programwhile {
         System.out.println("isi gelas sekarang : " + isiGelas + "ml");
         System.out.println("isi gelas ketika penuh : " + penuh + "ml");
 
-        while (isiGelas != penuh){
+        do {
+
             isiGelas++;
             System.out.println("sedang mengisi gelas..");
             System.out.println("isi gelas sekarang : " + isiGelas + "ml");
 
-        }
+        }while (isiGelas != penuh);
 
         System.out.println("finale: isi gelas sekarang : " + isiGelas + "ml");
 
     }
 }
+
