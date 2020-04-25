621660a125052687e96c03af68ba15bd542216fa
arifkadam <62461132+arifkadam@users.noreply.github.com>
2020-04-25 09:18:46 +0700

    isi gelas sekarang : isi gelas ketika penuh:

---
+++ src/perulangan/programwhile.java
@@ -0,0 +1,12 @@
+package perulangan;
+
+public class programwhile {
+    public static void main(String[] args) {
+        int isiGelas = 0;
+        int penuh = 225;
+
+        System.out.println("isi gelas sekarang : " + isiGelas + "ml");
+        System.out.println("isi gelas ketika penuh : " + penuh + "ml");
+
+    }
+}
