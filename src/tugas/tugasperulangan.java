27adb84f3dc9bc1fbfafa40f3b474913d29a95d1
arifkadam <62461132+arifkadam@users.noreply.github.com>
2020-04-25 13:51:49 +0700

    isi kotak

--- src/tugas/tugasperulangan.java
+++ src/tugas/tugasperulangan.java
@@ -2,7 +2,19 @@ package tugas;
 
 public class tugasperulangan {
     public static void main(String[] args) {
-        int isibotol = 0;
+        int isikotak = 0;
         int perkotak = 50;
-       
+
+        System.out.println("isi gelas sekarang : " + isikotak+ "ml");
+        System.out.println("isi gelas ketika penuh : " + perkotak+ "ml");
+
+        do {
+
+            isikotak.println("sedang mengisi gelas..");
+            System.out.println("isi gelas sekarang : " + isikotak + "ml");
+
+        } while (isikotak != perkotak);
+
+        System.out.println("finale: isi gelas sekarang : " + isikotak + "ml");
+    }
 }
