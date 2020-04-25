4b75650753e6287247e45549162cd812e41ca3d0
arifkadam <62461132+arifkadam@users.noreply.github.com>
2020-04-25 09:53:39 +0700

    nilaimahasiswadouble

--- src/array/programarray.java
+++ src/array/programarray.java
@@ -1,6 +1,15 @@
 package array;
 
 public class programarray {
-    String matakuliah[];
-    matakuliah = new String[10];
-}
+    public static void main(String[] args) {
+        double[][] nilaimahasiswa = new double[5][30];
+
+        nilaimahasiswa[0][0] = 80;
+        nilaimahasiswa[1][0] = 90;
+        nilaimahasiswa[2][0] = 75;
+        nilaimahasiswa[3][0] = 70;
+
+        nilaimahasiswa[4][0] = nilaimahasiswa[0][0] * bobot [0] +
+            nilaimahasiswa[1][0] * bobot [1] +
+            nilaimahasiswa[2][0] * bobot [2] +
+            nilaimahasiswa[3][0] * bobot [3];
