3049359ee8322430c5c1529ecd29230e132a45b5
arifkadam <62461132+arifkadam@users.noreply.github.com>
2020-04-25 14:11:05 +0700

    hasil nama dan NPM

--- src/tugas/tugasarray.java
+++ src/tugas/tugasarray.java
@@ -4,6 +4,13 @@ public class tugasarray {
     public static void main(String[] args) {
 
         String[][] kontak = {{"Arif,18630333"}, {"dony,1863987"}, {"bibi,1863234"}};
-        
+
+        for (int x = 0; x < kontak.length; x++) {
+            System.out.println("Nama: " + kontak[x][0]);
+            System.out.println("NPM: " + kontak[x][1]);
+            System.out.println("-----------------------------");
+        }
+
+        }
     }
 
