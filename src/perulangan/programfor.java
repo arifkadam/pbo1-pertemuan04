f7b244c62d3982bf6e893f518f21d2b321079818
arifkadam <62461132+arifkadam@users.noreply.github.com>
2020-04-25 09:34:46 +0700

    isi gelas kurang dari penuh

--- src/perulangan/programfor.java
+++ src/perulangan/programfor.java
@@ -5,4 +5,16 @@ public class programfor {
         int isiGelas = 0;
         int penuh = 225;
 
+        System.out.println("isi gelas sekarang : " + isiGelas + "ml");
+        System.out.println("isi gelas ketika penuh : " + penuh + "ml");
+
+        for (isiGelas = 0 ; isiGelas <= penuh ; isiGelas++){
+            System.out.println("sedang mengisi gelas..");
+            System.out.println("isi gelas sekarang : " + isiGelas + "ml");
+
+        }
+
+        System.out.println("finale: isi gelas sekarang : " + isiGelas + "ml");
+
     }
+}
