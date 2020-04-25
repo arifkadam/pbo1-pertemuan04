b2dea605a87f8be73c4ff2045581cb24bc894006
arifkadam <62461132+arifkadam@users.noreply.github.com>
2020-04-25 09:59:50 +0700

    nilai si mahasiswa 1

--- src/array/programarray.java
+++ src/array/programarray.java
@@ -13,3 +13,15 @@ public class programarray {
             nilaimahasiswa[1][0] * bobot [1] +
             nilaimahasiswa[2][0] * bobot [2] +
             nilaimahasiswa[3][0] * bobot [3];
+
+        System.out.println("nilai si mahasiswa 1 : " +nilaimahasiswa[4][0]);
+
+        nilaimahasiswa[0][1]= 80;
+        nilaimahasiswa[1][1]= 90;
+        nilaimahasiswa[2][1]= 75;
+        nilaimahasiswa[3][1]= 70;
+
+        nilaimahasiswa[4][1] = 0;
+        for (int i = 0 ; i<=3 ; i++) {
+            nilaimahasiswa[4][1] += nilaimahasiswa[i][1] * bobot[i];
+        }
